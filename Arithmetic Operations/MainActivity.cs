363e8959902f472bf131dbde2d0d4edca6a6365e
visualstudio <visualstudio@122-PC31>
2019-02-14 11:12:25 -0500

    Final Commit

--- Arithmetic Operations/MainActivity.cs
+++ Arithmetic Operations/MainActivity.cs
@@ -12,17 +12,52 @@ namespace Arithmetic_Operations
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-
+        TextView vnum1, vnum2, voperation, vresult;
+        EditText tnum1,tnum2,tresult;
+        Button bplus, bsub,bmul,bdiv;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
+            vnum1 = (TextView)FindViewById(Resource.Id.tvnum1);
+            vnum2 = (TextView)FindViewById(Resource.Id.tvnum2);
+            vresult = (TextView)FindViewById(Resource.Id.tvresult);
+            tnum1 = (EditText)FindViewById(Resource.Id.etnum1);
+            tnum2 = (EditText)FindViewById(Resource.Id.etnum2);
+            tresult = (EditText)FindViewById(Resource.Id.etresult);
+            bplus = (Button)FindViewById(Resource.Id.bnplus);
+            bsub = (Button)FindViewById(Resource.Id.bnsub);
+            bmul = (Button)FindViewById(Resource.Id.bnmul);
+            bdiv = (Button)FindViewById(Resource.Id.bndiv);
 
-            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
-
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
+            bplus.Click += delegate
+            {
+                double a = double.Parse(tnum1.Text);
+                double b = double.Parse(tnum2.Text);
+                double r = a+b;
+                tresult.Text = r.ToString();
+            };
+            bsub.Click += delegate
+            {
+                double a = double.Parse(tnum1.Text);
+                double b = double.Parse(tnum2.Text);
+                double r = a - b;
+                tresult.Text = r.ToString();
+            };
+            bmul.Click += delegate
+            {
+                double a = double.Parse(tnum1.Text);
+                double b = double.Parse(tnum2.Text);
+                double r = a * b;
+                tresult.Text = r.ToString();
+            };
+            bdiv.Click += delegate
+            {
+                double a = double.Parse(tnum1.Text);
+                double b = double.Parse(tnum2.Text);
+                double r = a / b;
+                tresult.Text = r.ToString();
+            };
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
